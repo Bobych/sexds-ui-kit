6b012bd501341c18c437d14090837e94c6e36c5e
nikit <nikitagoder@gmail.com>
2025-02-26 13:34:58 +0300

    !feat add atoms (colors, tokens, numbers, typography)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/styles.scss";
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, ullam!</h1>
+        </div>
+    );
+};
+
+export default App;
