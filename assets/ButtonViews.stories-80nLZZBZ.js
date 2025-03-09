606a9e953b873f85e875ffca87f95b9ca0b14cd9
Bobych <Bobych@users.noreply.github.com>
2025-03-09 14:58:50 +0000

    deploy: aae45e61fedc28f534d34e602af3023bc8acfc32

---
+++ assets/ButtonViews.stories-80nLZZBZ.js
@@ -0,0 +1,61 @@
+import{I as s,B as b,a as x,b as v,c as I,d as H}from"./constants-BvM-wTim.js";import"./jsx-runtime-z8MfsBtr.js";import"./index-C9rmetsa.js";import"./index-BofL8h0e.js";const E={title:"SEXDS/Button/View",component:H,argTypes:{variant:{control:"select",options:I},view:{control:"select",options:v},size:{control:"select",options:x},status:{control:"select",options:b},leftIcon:{control:"select",options:s},rightIcon:{control:"select",options:s},selected:{control:"boolean"},disabled:{control:"boolean"},loading:{control:"boolean"},children:{control:"text"},href:{control:"text"},target:{control:"select",options:["_blank","_self","_parent","_top"]}}},e={args:{children:"Text",variant:"default",view:"default",size:"m",status:"default",leftIcon:"HandEye",rightIcon:"Hand",disabled:!1,loading:!1,selected:!1,href:"https://sportexclub.com",target:"_blank"}},t={args:{children:"Text",variant:"invert",view:"tone",size:"m",status:"default",leftIcon:"HandEye",rightIcon:"Hand",disabled:!1,loading:!1,selected:!1,href:"https://sportexclub.com",target:"_blank"}},a={args:{children:"Text",variant:"accent",view:"outline",size:"m",status:"default",leftIcon:"HandEye",rightIcon:"Hand",disabled:!1,loading:!1,selected:!1,href:"https://sportexclub.com",target:"_blank"}},n={args:{children:"Text",variant:"success",view:"text",size:"m",status:"default",leftIcon:"HandEye",rightIcon:"Hand",disabled:!1,loading:!1,selected:!1,href:"https://sportexclub.com",target:"_blank"}};var o,r,l;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    children: "Text",
+    variant: "default",
+    view: "default",
+    size: "m",
+    status: "default",
+    leftIcon: "HandEye",
+    rightIcon: "Hand",
+    disabled: false,
+    loading: false,
+    selected: false,
+    href: "https://sportexclub.com",
+    target: "_blank"
+  }
+}`,...(l=(r=e.parameters)==null?void 0:r.docs)==null?void 0:l.source}}};var c,i,d;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: "Text",
+    variant: "invert",
+    view: "tone",
+    size: "m",
+    status: "default",
+    leftIcon: "HandEye",
+    rightIcon: "Hand",
+    disabled: false,
+    loading: false,
+    selected: false,
+    href: "https://sportexclub.com",
+    target: "_blank"
+  }
+}`,...(d=(i=t.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var f,u,p;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    children: "Text",
+    variant: "accent",
+    view: "outline",
+    size: "m",
+    status: "default",
+    leftIcon: "HandEye",
+    rightIcon: "Hand",
+    disabled: false,
+    loading: false,
+    selected: false,
+    href: "https://sportexclub.com",
+    target: "_blank"
+  }
+}`,...(p=(u=a.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};var m,g,h;n.parameters={...n.parameters,docs:{...(m=n.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: "Text",
+    variant: "success",
+    view: "text",
+    size: "m",
+    status: "default",
+    leftIcon: "HandEye",
+    rightIcon: "Hand",
+    disabled: false,
+    loading: false,
+    selected: false,
+    href: "https://sportexclub.com",
+    target: "_blank"
+  }
+}`,...(h=(g=n.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};const k=["Default","Tone","Outline","Text"];export{e as Default,a as Outline,n as Text,t as Tone,k as __namedExportsOrder,E as default};
